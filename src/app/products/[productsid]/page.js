973d8e96d0d9f5d011d0192082b2ae4c9e3d0493
brainytech <janivansh2002@gmail.com>
2024-08-22 00:27:58 +0530

    first commit

---
+++ src/app/products/[productsid]/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function page({ params }) {
+
+    return (
+        <div>page Details</div>
+    )
+}
+
+export default page
---
+++ src/app/products/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    Page
+    </>
+  )
+}
+
+export default page
